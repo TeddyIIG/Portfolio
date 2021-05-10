2ff47aae5cb8b9f4d42714575d6212c548c1e70a
TeddyIIG <shehankafernando@gmail.com>
2021-05-10 20:43:09 +0530

    added some info 5/10

--- Portfolio_app/static/js/chatbot.js
+++ Portfolio_app/static/js/chatbot.js
@@ -1,13 +1,12 @@
-
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.setAttribute('crossorigin','*');
 s1.src='https://embed.tawk.to/5e47be34298c395d1ce81006/default';
 s1.charset='UTF-8';
-
+s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
+
 
 
