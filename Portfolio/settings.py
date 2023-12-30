1e98b75e2640eb8e36c962fdb9d3ef49abe55003
Shehanka Fernando <shehankafernando@gmail.com>
2023-12-31 00:50:04 +0530

    Added new gitignore and configured settings.py file

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -80,19 +80,24 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'Portfolio/../db.sqlite3'),
-
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'portfolio',
-        # 'USER': 'shehanka',
-        # 'PASSWORD': 'Sharmaine1',
-        # 'HOST': '192.3.13.96',
-        # 'PORT': '3306',
+if os.getenv('DB_ENV') == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASS'),
+            'HOST': '192.3.13.96',
+            'PORT': '3306',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }     
 
 
 # Password validation
